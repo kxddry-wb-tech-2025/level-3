5f25b4a5718a085f62a24cf4b497612304b4dc26
Ivan Kudryavtsev <ivakudry@gmail.com>
2025-08-15 20:29:24 +0300

    proto done

--- 1_delayed_notifier/internal/storage/storage.go
+++ 1_delayed_notifier/internal/storage/storage.go
@@ -1,14 +1,9 @@
 package storage
 
 import (
-	"errors"
 	"time"
 
 	"github.com/wb-go/wbf/retry"
-)
-
-var (
-	ErrNoSubscription = errors.New("no subscription with that id found")
 )
 
 var Strategy = retry.Strategy{Attempts: 3, Backoff: 2, Delay: 5 * time.Second}
