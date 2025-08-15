2b84d5197d90fd8b857321ef91f10ddd517e6f4b
Ivan Kudryavtsev <ivakudry@gmail.com>
2025-08-15 17:48:14 +0300

    storage

---
+++ 1_delayed_notifier/internal/models/notifications.go
@@ -0,0 +1,27 @@
+package models
+
+import "time"
+
+const (
+	StatusCreated = iota + 1
+	StatusSent
+	StatusReceived
+	StatusCanceled
+)
+
+const (
+	ChannelEmail = iota + 1
+	ChannelTelegram
+)
+
+type Notification struct {
+	UserID    int64     `json:"user_id"`
+	ChannelID int64     `json:"channel_id"`
+	SendAt    time.Time `json:"send_at"`
+	Payload   payload   `json:"payload"`
+}
+
+type payload struct {
+	Subject string `json:"subject"`
+	Body    string `json:"body"`
+}
