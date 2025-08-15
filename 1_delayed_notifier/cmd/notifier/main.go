75e69deae962dd5a3da880bbe004e17ba4bb5b47
Ivan Kudryavtsev <ivakudry@gmail.com>
2025-08-15 11:22:32 +0300

    cfg

--- 1_delayed_notifier/cmd/notifier/main.go
+++ 1_delayed_notifier/cmd/notifier/main.go
@@ -1,10 +1,22 @@
 package main
 
-import "github.com/wb-go/wbf/zlog"
+import (
+	"os"
+
+	"github.com/wb-go/wbf/config"
+	"github.com/wb-go/wbf/zlog"
+)
 
 func main() {
 	zlog.Init()
 	log := zlog.Logger
 	log.Debug().Msg("debug enabled")
+
+	cfg := config.New()
+	err := cfg.Load("config.yaml")
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to load config.yaml")
+		os.Exit(1)
+	}
 
 }
