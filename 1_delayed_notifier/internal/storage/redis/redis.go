47a02e4d265e998a96c80d1c5c94960c7b792608
Ivan Kudryavtsev <ivakudry@gmail.com>
2025-08-15 18:52:13 +0300

    redis

---
+++ 1_delayed_notifier/internal/storage/redis/redis.go
@@ -0,0 +1,39 @@
+package redis
+
+import (
+	"context"
+	"delayed-notifier/internal/models"
+	"delayed-notifier/internal/storage"
+	"encoding/json"
+
+	"github.com/wb-go/wbf/redis"
+)
+
+type Storage struct {
+	rdb *redis.Client
+}
+
+func New(addr, password string, db int) *Storage {
+	return &Storage{
+		rdb: redis.New(addr, password, db),
+	}
+}
+
+func (s *Storage) key(id string) string { return "notif:" + id }
+
+func (s *Storage) Set(ctx context.Context, st models.NotificationStatus) error {
+	data, _ := json.Marshal(st)
+	return s.rdb.SetWithRetry(ctx, storage.Strategy, s.key(st.ID), data)
+}
+
+func (s *Storage) Get(ctx context.Context, id string) (*models.NotificationStatus, error) {
+	val, err := s.rdb.Get(ctx, s.key(id))
+	if err != nil {
+		return nil, err
+	}
+	var st models.NotificationStatus
+	if err := json.Unmarshal([]byte(val), &st); err != nil {
+		return nil, err
+	}
+	return &st, nil
+}
