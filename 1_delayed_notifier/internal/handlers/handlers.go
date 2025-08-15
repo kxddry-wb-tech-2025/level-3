e185f78cfdbc9c57f213969a6c9c9418a423d186
Ivan Kudryavtsev <ivakudry@gmail.com>
2025-08-15 19:49:00 +0300

    mumumumum

--- 1_delayed_notifier/internal/handlers/handlers.go
+++ 1_delayed_notifier/internal/handlers/handlers.go
@@ -1,16 +1,1 @@
 package handlers
-
-import (
-	"context"
-	"delayed-notifier/internal/models"
-)
-
-type Storage interface {
-	Add(ctx context.Context, notify *models.NotificationCreate) (int64, error)
-	Update(ctx context.Context, id int64, status int) error
-	Get(ctx context.Context, id int64) (*models.Notification, error)
-}
-
-type Publisher interface {
-	Publish(msg models.Notification) error
-}
