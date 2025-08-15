843e7b33064e0b95ce35b28e649a75ae989f1976
Ivan Kudryavtsev <ivakudry@gmail.com>
2025-08-15 18:52:45 +0300

    rabbitmq

--- 1_delayed_notifier/internal/broker/rabbitmq/rabbitmq.go
+++ 1_delayed_notifier/internal/broker/rabbitmq/rabbitmq.go
@@ -1,39 +1,89 @@
 package rabbitmq
 
-import amqp "github.com/rabbitmq/amqp091-go"
+import (
+	"context"
+	"delayed-notifier/internal/models"
+	"encoding/json"
+	"fmt"
+	"time"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+const (
+	exchangeReady = "delayed_notifier.ready"
+	queueReady    = "delayed_notifier.ready"
+	queueDelayed  = "delayed_notifier.delayed"
+)
 
 type RabbitMQ struct {
 	conn *amqp.Connection
-	q    *amqp.Queue
 	ch   *amqp.Channel
 }
 
-func New(dsn, queueName string) (*RabbitMQ, error) {
-	conn, err := amqp.Dial(dsn)
+func New(url string) (*RabbitMQ, error) {
+	conn, err := amqp.Dial(url)
 	if err != nil {
 		return nil, err
 	}
 
 	ch, err := conn.Channel()
 	if err != nil {
+		_ = conn.Close()
 		return nil, err
 	}
 
-	q, err := ch.QueueDeclare(queueName, true, false, false, false, nil)
-	if err != nil {
+	mq := &RabbitMQ{conn: conn, ch: ch}
+	if err := mq.setup(); err != nil {
+		_ = conn.Close()
+		_ = ch.Close()
 		return nil, err
 	}
-
-	return &RabbitMQ{conn: conn, q: &q, ch: ch}, nil
+	return mq, nil
 }
 
-func (r *RabbitMQ) Close() error {
-	return r.conn.Close()
+func (r *RabbitMQ) setup() error {
+	if err := r.ch.ExchangeDeclare(exchangeReady, "fanout", true, false, false, false, nil); err != nil {
+		return err
+	}
+	if _, err := r.ch.QueueDeclare(queueReady, true, false, false, false, nil); err != nil {
+		return err
+	}
+	if err := r.ch.QueueBind(queueReady, "", exchangeReady, false, nil); err != nil {
+		return err
+	}
+	// Очередь delayed с DLX на ready exchange
+	args := amqp.Table{
+		"x-dead-letter-exchange": exchangeReady,
+	}
+	if _, err := r.ch.QueueDeclare(queueDelayed, true, false, false, false, args); err != nil {
+		return err
+	}
+	return nil
 }
 
-func (r *RabbitMQ) Publish(exchange string, msg []byte) error {
-	return r.ch.Publish(exchange, r.q.Name, false, false, amqp.Publishing{
-		ContentType: "application/json",
-		Body:        msg,
+func (r *RabbitMQ) Close() {
+	if r.ch != nil {
+		_ = r.ch.Close()
+	}
+	if r.conn != nil {
+		_ = r.conn.Close()
+	}
+}
+
+func (r *RabbitMQ) PublishDelayed(ctx context.Context, n models.Notification, delay time.Duration) error {
+	body, _ := json.Marshal(n)
+	exp := fmt.Sprintf("%d", int(delay.Milliseconds()))
+	return r.ch.PublishWithContext(ctx, "", queueDelayed, false, false, amqp.Publishing{
+		ContentType:  "application/json",
+		Body:         body,
+		DeliveryMode: amqp.Persistent,
+		Expiration:   exp,
+		Headers: amqp.Table{
+			"x-attempt": n.Attempt,
+		},
+		Timestamp: time.Now(),
+		MessageId: fmt.Sprintf("%d", n.ID),
+		Type:      "notification",
 	})
 }
