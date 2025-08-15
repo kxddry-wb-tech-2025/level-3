ff4728261054c67a0b8ec65b2143cb6daa63f50b
Ivan Kudryavtsev <ivakudry@gmail.com>
2025-08-15 19:51:19 +0300

    get

---
+++ 1_delayed_notifier/internal/handlers/get.go
@@ -0,0 +1,31 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func (s *Server) GetNotification() func(c *gin.Context) {
+	return func(c *gin.Context) {
+		id := c.Param("id")
+		if id == "" {
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+				"message": "id is required",
+			})
+			return
+		}
+
+		st, err := s.store.Get(c.Copy(), id)
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		c.JSON(http.StatusOK, st)
+		return
+
+	}
+}
