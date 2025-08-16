256341e13442b41fe2c3a579e68cfa0a41ce3615
Ivan Kudryavtsev <ivakudry@gmail.com>
2025-08-16 16:45:48 +0300

    worker

--- 1_delayed_notifier/internal/processor/worker.go
+++ 1_delayed_notifier/internal/processor/worker.go
@@ -4,14 +4,16 @@ import (
 	"context"
 	"delayed-notifier/internal/models"
 	"fmt"
+	"time"
 
+	"github.com/kxddry/wbf/retry"
 	"github.com/rs/zerolog"
 )
 
 // Broker can be something like Kafka streams or RabbitMQ. It's used for working with delayed notifications.
 type Broker interface {
 	Publish(ctx context.Context, n models.Notification) error
-	Consume(ctx context.Context) (<-chan models.QueuePayload, error)
+	Consume(ctx context.Context, queueName string) (<-chan models.QueuePayload, error)
 }
 
 // StatusStore is used as an interface to store and get statuses
@@ -59,5 +61,76 @@ func (np *NotificationProcessor) Start(ctx context.Context) {
 func (np *NotificationProcessor) consume(ctx context.Context, channel string) error {
 	queueName := fmt.Sprintf("delayed_notifier.%s", channel)
 
-	msgs, err := np.b.Consume(ctx)
+	msgs, err := np.b.Consume(ctx, queueName)
+	if err != nil {
+		return fmt.Errorf("failed to regiter consumer for %s: %v", channel, err)
+	}
+
+	np.log.Info().Msg("started consumer for channel " + channel)
+
+	for {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case m, ok := <-msgs:
+			if !ok {
+				return fmt.Errorf("channel closed")
+			}
+			if err := np.process(ctx, m.Notify); err != nil {
+				np.log.Err(err).Msg("failed to process message")
+				m.Discard()
+			} else {
+				m.Commit()
+			}
+		}
+	}
+}
+
+func (np *NotificationProcessor) process(ctx context.Context, note *models.Notification) error {
+
+	if status, err := np.store.Get(ctx, note.ID); err == nil && status.Status == models.StatusFailed {
+		return nil
+	}
+
+	ch := note.Channel
+	var err error
+
+	if err = np.store.Set(ctx, models.NotificationStatus{
+		ID:        note.ID,
+		Status:    models.StatusReceived,
+		UpdatedAt: time.Now(),
+	}); err != nil {
+		np.log.Err(err).Msg("failed to change notification status")
+		return fmt.Errorf("failed to change notification status to received")
+	}
+
+	switch ch {
+	case "telegram":
+		err = np.tg.Send(ctx, *note)
+	default:
+		return fmt.Errorf("unknown channel %s", ch)
+	}
+
+	if err != nil {
+		if err = np.store.Set(ctx, models.NotificationStatus{
+			ID:        note.ID,
+			Status:    models.StatusFailed,
+			UpdatedAt: time.Now(),
+		}); err != nil {
+			np.log.Err(err).Msg("failed to change notification status to failed")
+			return fmt.Errorf("failed to change notification status to failed")
+		}
+
+		if err = retry.Do(func() error {
+			note.Attempt++
+			return np.b.Publish(ctx, *note)
+		}, retry.Strategy{
+			Attempts: 3,
+			Delay:    2,
+			Backoff:  2,
+		}); err != nil {
+			np.log.Err(err).Msg("PERMANENT FAILURE !!!")
+		}
+	}
+	return nil
 }
