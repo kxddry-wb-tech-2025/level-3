58d86cdcef7151d49f002476d8af7a32e1f76c4c
Ivan Kudryavtsev <ivakudry@gmail.com>
2025-08-17 13:41:19 +0300

    POST handled

---
+++ 2_shortener/internal/validator/validator.go
@@ -0,0 +1,15 @@
+package validator
+
+import "github.com/go-playground/validator/v10"
+
+type Validator struct {
+	validate *validator.Validate
+}
+
+func New() *Validator {
+	return &Validator{validate: validator.New()}
+}
+
+func (v *Validator) URL(url string) error {
+	return v.validate.Var(url, "required,url")
+}
