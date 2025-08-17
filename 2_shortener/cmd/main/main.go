ca458ca7759cde581cbf7f5e8809cc017c241b29
Ivan Kudryavtsev <ivakudry@gmail.com>
2025-08-17 14:20:01 +0300

    add example configuration and update config loading mechanism

--- 2_shortener/cmd/main/main.go
+++ 2_shortener/cmd/main/main.go
@@ -2,22 +2,31 @@ package main
 
 import (
 	"context"
+	"os"
 	"shortener/internal/api"
 	"shortener/internal/storage/postgres"
 	"shortener/internal/validator"
 
 	"github.com/kxddry/wbf/config"
 	"github.com/kxddry/wbf/zlog"
+	"github.com/subosito/gotenv"
 )
 
 func main() {
+	_ = gotenv.Load(".env")
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
 	zlog.Init()
 
 	cfg := config.New()
-	cfg.Load("./config.yaml")
+	if err := cfg.Load(os.Getenv("CONFIG_PATH")); err != nil {
+		zlog.Logger.Info().Msg("CONFIG_PATH is not set, searching for config.yaml in the current directory")
+		if err := cfg.Load("./config.yaml"); err != nil {
+			zlog.Logger.Fatal().Err(err).Msg("failed to load config")
+			os.Exit(1)
+		}
+	}
 
 	store, err := postgres.New(ctx, cfg.GetString("postgres.master"), nil)
 	if err != nil {
