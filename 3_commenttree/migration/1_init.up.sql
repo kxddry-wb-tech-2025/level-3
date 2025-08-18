00cfd6d6d5414ff09a64225863e1a2110b8282ca
Ivan Kudryavtsev <ivakudry@gmail.com>
2025-08-18 13:04:20 +0300

    fix psql syntax in up migration

--- 3_commenttree/migration/1_init.up.sql
+++ 3_commenttree/migration/1_init.up.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS comments (
 	id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
 	content TEXT NOT NULL,
 	parent_id UUID REFERENCES comments(id) ON DELETE CASCADE,
-	created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+	created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
 
 CREATE INDEX IF NOT EXISTS idx_comments_parent_id ON comments(parent_id);
