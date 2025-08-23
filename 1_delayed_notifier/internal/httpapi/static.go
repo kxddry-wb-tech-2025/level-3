e15c4320ff6ed6653305649123834422d71bc402
Ivan Kudryavtsev <ivakudry@gmail.com>
2025-08-23 11:38:27 +0300

    use ctx instead of context.Background(), fix some illogical choices

--- 1_delayed_notifier/internal/httpapi/static.go
+++ 1_delayed_notifier/internal/httpapi/static.go
@@ -9,7 +9,7 @@ import (
 // ServeStatic registers a handler that serves only the root index.html from the given directory.
 func ServeStatic(r *ginext.Engine, routePrefix string, dir string) {
 	indexPath := dir + "/index.html"
-	r.GET("/", func(c *ginext.Context) {
+	r.GET(routePrefix, func(c *ginext.Context) {
 		http.ServeFile(c.Writer, c.Request, indexPath)
 	})
 }
