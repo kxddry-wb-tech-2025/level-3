afc583fb81d48501e5add8cd011ebcdc071d003d
Ivan Kudryavtsev <ivakudry@gmail.com>
2025-08-25 11:10:31 +0300

    rise and grind | add methods

--- 6_salestracker/src/internal/delivery/server.go
+++ 6_salestracker/src/internal/delivery/server.go
@@ -6,24 +6,31 @@ import (
 	"salestracker/src/internal/models"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"github.com/google/uuid"
 	"github.com/kxddry/wbf/ginext"
 )
 
 type Service interface {
-	Post(ctx context.Context, req models.PostRequest) (models.PostResponse, error)
+	PostItem(ctx context.Context, req models.PostRequest) (models.PostResponse, error)
+	GetItems(ctx context.Context) ([]models.Item, error)
+	PutItem(ctx context.Context, id string, req models.PutRequest) (models.PutResponse, error)
 }
 
 // Server is a struct that contains the router.
 type Server struct {
 	r   *ginext.Engine
 	svc Service
+	v   *validator.Validate
 }
 
 func New(svc Service) *Server {
 	r := ginext.New()
+	v := validator.New()
 	srv := &Server{
 		r:   r,
 		svc: svc,
+		v:   v,
 	}
 
 	srv.registerRoutes()
@@ -41,7 +48,45 @@ func (s *Server) registerRoutes() {
 			return
 		}
 
-		res, err := s.svc.Post(c.Request.Context(), req)
+		res, err := s.svc.PostItem(c.Request.Context(), req)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusOK, res)
+	})
+
+	r.GET("/items", func(c *ginext.Context) {
+		items, err := s.svc.GetItems(c.Request.Context())
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusOK, items)
+	})
+
+	r.PUT("/items/:id", func(c *ginext.Context) {
+		var req models.PutRequest
+		if err := c.ShouldBindJSON(&req); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		if err := s.v.Struct(req); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		// validate uuid
+		id := c.Param("id")
+		if _, err := uuid.Parse(id); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "invalid id"})
+			return
+		}
+
+		res, err := s.svc.PutItem(c.Request.Context(), id, req)
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 			return
---
+++ 6_salestracker/src/internal/models/update_item.go
@@ -0,0 +1,7 @@
+package models
+
+// PutRequest can be the same as PostRequest. The ID flag is provided in the params anyway.
+type PutRequest PostRequest
+
+// PutResponse can be the same as PostResponse.
+type PutResponse PostResponse
